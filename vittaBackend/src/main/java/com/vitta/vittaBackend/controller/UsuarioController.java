e808ff3b6abd5c968b3ebbe3a3a214720deeafba
Gabriel Silva <ogabrieusiuva@gmail.com>
2025-10-11 19:37:56 -0300

    Remoção de método que listava usuários.

--- vittaBackend/src/main/java/com/vitta/vittaBackend/controller/UsuarioController.java
+++ vittaBackend/src/main/java/com/vitta/vittaBackend/controller/UsuarioController.java
@@ -6,10 +6,8 @@ import com.vitta.vittaBackend.dto.response.usuario.*;
 import com.vitta.vittaBackend.dto.security.RecoveryJwtTokenDto;
 import com.vitta.vittaBackend.dto.security.UsuarioLoginDto;
 import com.vitta.vittaBackend.security.UserDetailsImpl;
-import com.vitta.vittaBackend.service.AgendaService;
 import com.vitta.vittaBackend.service.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -29,16 +27,13 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private UsuarioService usuarioService;
-    private AgendaService agendaService;
 
     /**
      * Construtor para injeção de dependências dos serviços de Usuário и Agenda.
      * @param usuarioService Serviço com a lógica de negócio para Usuários.
-     * @param agendaService Serviço com a lógica de negócio para a Agenda.
      */
-    public UsuarioController(UsuarioService usuarioService, AgendaService agendaService) {
+    public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
-        this.agendaService = agendaService;
     }
 
     /**
@@ -51,7 +46,7 @@ public class UsuarioController {
      * @return Resposta com status 201 Created.
      */
     @PostMapping("/cadastrar")
-    @Operation(summary = "Cadastrar Usuário", description = "Endpoint para cadastrar Usuários, com SecurityJWT.")
+    @Operation(summary = "Cadastrar Usuário.", description = "Endpoint para cadastrar Usuários, com SecurityJWT.")
     public ResponseEntity<Void> criarUsuario(@RequestBody UsuarioDTORequest usuarioDTORequest) {
         usuarioService.criarUsuario(usuarioDTORequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
@@ -68,7 +63,7 @@ public class UsuarioController {
      * @return O token JWT para ser usado nas próximas requisições.
      */
     @PostMapping("/login")
-    @Operation(summary = "Login de Usuário", description = "Endpoint para fazer o Login de 1 Usuário cadastrado no Banco.")
+    @Operation(summary = "Login de Usuário.", description = "Endpoint para fazer o Login de 1 Usuário cadastrado no Banco.")
     public ResponseEntity<RecoveryJwtTokenDto> autenticarUsuarioSecurity(@RequestBody UsuarioLoginDto usuarioLoginDTO) {
         RecoveryJwtTokenDto token = usuarioService.autenticarUsuario(usuarioLoginDTO);
         return new ResponseEntity<>(token, HttpStatus.OK);
@@ -80,7 +75,7 @@ public class UsuarioController {
      * @return O perfil completo do usuário logado.
      */
     @GetMapping("/buscarMeuPerfil")
-    @Operation(summary = "Buscar meu perfil", description = "Endpoint para o usuário logado buscar seus próprios dados de perfil.")
+    @Operation(summary = "Buscar meu perfil.", description = "Endpoint para o usuário logado buscar seus próprios dados de perfil.")
     public ResponseEntity<UsuarioDTOResponse> buscarMeuPerfil(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         Integer usuarioId = userDetails.getUserId();
         UsuarioDTOResponse usuarioDTOResponse = usuarioService.buscarMeuPerfil(usuarioId);
@@ -95,7 +90,7 @@ public class UsuarioController {
      * @return O perfil do usuário com os dados atualizados.
      */
     @PutMapping("/atualizarMeuPerfil")
-    @Operation(summary = "Atualizar meu perfil", description = "Endpoint para o usuário logado atualizar seus dados de perfil.")
+    @Operation(summary = "Atualizar meu perfil.", description = "Endpoint para o usuário logado atualizar seus dados de perfil.")
     public ResponseEntity<UsuarioDTOResponse> atualizarUsuarioPorId(
             @RequestBody @Valid UsuarioAtualizarDTORequest usuarioAtualizarDTORequest,
             @AuthenticationPrincipal UserDetailsImpl userDetails
@@ -111,7 +106,7 @@ public class UsuarioController {
      * @return Resposta sem conteúdo.
      */
     @DeleteMapping("/deletarMinhaConta")
-    @Operation(summary = "Deletar minha conta", description = "Endpoint para o usuário logado deletar (logicamente) sua própria conta.")
+    @Operation(summary = "Deletar minha conta.", description = "Endpoint para o usuário logado deletar (logicamente) sua própria conta.")
     public ResponseEntity<Void> deletarUsuario(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         Integer usuarioId = userDetails.getUserId();
         usuarioService.deletarMinhaConta(usuarioId);
@@ -125,7 +120,7 @@ public class UsuarioController {
      * @param userDetails Detalhes do usuário injetados pelo Spring Security.
      */
     @GetMapping("/listarMeusAgendamentos")
-    @Operation(summary = "Listar meus agendamentos", description = "Busca o usuário logado com sua lista de agendamentos cadastrados.")
+    @Operation(summary = "Listar meus agendamentos.", description = "Busca o usuário logado com sua lista de agendamentos cadastrados.")
     public ResponseEntity<UsuarioAgendamentosDTOResponse> buscarMeusAgendamentos(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         Integer usuarioId = userDetails.getUserId();
         return ResponseEntity.ok(usuarioService.buscarMeusAgendamentos(usuarioId));
@@ -136,7 +131,7 @@ public class UsuarioController {
      * @param userDetails Detalhes do usuário injetados pelo Spring Security.
      */
     @GetMapping("/listarMeusHistoricos")
-    @Operation(summary = "Listar meus históricos", description = "Busca o usuário logado com seu histórico de uso de medicamentos cadastrados.")
+    @Operation(summary = "Listar meus históricos.", description = "Busca o usuário logado com seu histórico de uso de medicamentos cadastrados.")
     public ResponseEntity<UsuarioHistoricoDTOResponse> buscarMeusHistoricos(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         Integer usuarioId = userDetails.getUserId();
         return ResponseEntity.ok(usuarioService.buscarMeusHistoricos(usuarioId));
@@ -147,7 +142,7 @@ public class UsuarioController {
      * @param userDetails Detalhes do usuário injetados pelo Spring Security.
      */
     @GetMapping("/listarMeusMedicamentos")
-    @Operation(summary = "Listar meus medicamentos", description = "Busca o usuário logado com sua lista de medicamentos cadastrados.")
+    @Operation(summary = "Listar meus medicamentos.", description = "Busca o usuário logado com sua lista de medicamentos cadastrados.")
     public ResponseEntity<UsuarioMedicamentosDTOResponse> buscarMeusMedicamentos(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         Integer usuarioId = userDetails.getUserId();
         return ResponseEntity.ok(usuarioService.buscarMeusMedicamentos(usuarioId));
@@ -158,7 +153,7 @@ public class UsuarioController {
      * @param userDetails Detalhes do usuário injetados pelo Spring Security.
      */
     @GetMapping("/listarMeusTratamentos")
-    @Operation(summary = "Listar meus tratamentos", description = "Busca o usuário logado com sua lista de tratamentos cadastrados.")
+    @Operation(summary = "Listar meus tratamentos.", description = "Busca o usuário logado com sua lista de tratamentos cadastrados.")
     public ResponseEntity<UsuarioTratamentosDTOResponse> buscarMeusTratamentos(@AuthenticationPrincipal UserDetailsImpl userDetails) {
         Integer usuarioId = userDetails.getUserId();
         return ResponseEntity.ok(usuarioService.buscarMeusTratamentos(usuarioId));
@@ -173,7 +168,7 @@ public class UsuarioController {
      * @return um {@link ResponseEntity} com uma mensagem de sucesso e o status HTTP 200 (OK).
      */
     @GetMapping("/test")
-    @Operation(summary = "Teste de Usuário", description = "Endpoint para verificar se um Usuário está autenticado.")
+    @Operation(summary = "Teste de Usuário.", description = "Endpoint para verificar se um Usuário está autenticado.")
     public ResponseEntity<String> getAuthenticationTest() {
         return new ResponseEntity<>("Autenticado com sucesso", HttpStatus.OK);
     }
@@ -188,32 +183,9 @@ public class UsuarioController {
      * @return um {@link ResponseEntity} com uma mensagem de sucesso para clientes e o status HTTP 200 (OK).
      */
     @GetMapping("/test/customer")
-    @Operation(summary = "Listar Role de Usuário", description = "Endpoint para verificar a Role do Usuário autenticado.")
+    @Operation(summary = "Listar Role de Usuário.", description = "Endpoint para verificar a Role do Usuário autenticado.")
     public ResponseEntity<String> getCustomerAuthenticationTest() {
         return new ResponseEntity<>("Cliente autenticado com sucesso", HttpStatus.OK);
     }
 
-    //    /**
-//     * Lista todos os usuários com status ATIVO.
-//     * @return ResponseEntity contendo uma lista de usuários e o status HTTP 200 OK.
-//     */
-//    @GetMapping("/listar")
-//    @Operation(summary = "Listar Usuários", description = "Endpoint para listar todos os Usuários.")
-//    public ResponseEntity <List<UsuarioDTOResponse>> listarUsuarios() { return ResponseEntity.ok(usuarioService.listarUsuariosAtivos()); }
-
-
-    /**
-     * Busca a agenda de medicamentos do dia para um usuário específico.
-     * @param usuarioId O ID do usuário para o qual a agenda será consultada.
-     * @return ResponseEntity com a lista de agendamentos do dia e status 200 OK,
-     * ou status 204 No Content se não houver agendamentos para o dia.
-     */
-//    @GetMapping("/{usuarioId}/agendaDoDia")
-//    public ResponseEntity<List<AgendaDoDiaDTOResponse>> getAgendaDoDia(@PathVariable Integer usuarioId) {
-//        List<AgendaDoDiaDTOResponse> agenda = agendaService.getAgendaDoDia(usuarioId);
-//        if (agenda.isEmpty()) {
-//            return ResponseEntity.noContent().build();
-//        }
-//        return ResponseEntity.ok(agenda);
-//    }
 }
--- vittaBackend/src/main/java/com/vitta/vittaBackend/service/TratamentoService.java
+++ vittaBackend/src/main/java/com/vitta/vittaBackend/service/TratamentoService.java
@@ -18,10 +18,8 @@ import com.vitta.vittaBackend.repository.UsuarioRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
